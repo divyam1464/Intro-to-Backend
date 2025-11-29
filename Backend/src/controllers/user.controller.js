7a2761401efc5138cbe636f3bf00894c35f6510b
Divyam Agarwal <divyam7246@gmail.com>
2025-11-29 17:11:28 +0530

    features changed

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,37 +1,83 @@
-import { User } from '../models/user.model.js';
+import { User } from "../models/user.model.js";
 
 const registerUser = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-        //basic validation
-        if ( !username || !email || !password ){
-            return res.status(400).json({message: "ALL FIELDS ARE IMPORTANT !"})
-        }
-
-        // check user already exist
-        const existing = await User.findOne({email: email.toLowerCase()})
-        if (existing){
-            return res.status(400).json({message: "User already exists !"})
-        }
-
-        // create user 
-        const user = await User.create({
-            username,
-            email: email.toLowerCase(),
-            password,
-            loggedIn: false,
-        });
-
-        res.status(201).json({
-            message: "User Registered Successfully.",
-            user: {id: user._id , email: user.email, username: user.username }
-        });
-    } catch (error) {
-        res.status(500).json({message:"Internal Server Error !", error: error.message})
+    //basic validation
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "ALL FIELDS ARE IMPORTANT !" });
     }
+
+    // check user already exist
+    const existing = await User.findOne({ email: email.toLowerCase() });
+    if (existing) {
+      return res.status(400).json({ message: "User already exists !" });
+    }
+
+    // create user
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password,
+      loggedIn: false,
+    });
+
+    res.status(201).json({
+      message: "User Registered Successfully.",
+      user: { id: user._id, email: user.email, username: user.username },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error !", error: error.message });
+  }
 };
 
-export {
-    registerUser
+const loginUser = async (req, res) => {
+  try {
+    //checking if the user already exists
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      email: email.toLowerCase(),
+    });
+    if (!user) {
+      return res.status(400).json({ message: "User Not Found" });
+    }
+    //compare the passwords
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch)
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    res.status(200).json({
+      message: "User Logged in",
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
+
+const logoutUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+    res.status(200).json({ message: "Logout Successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+};
+
+export { registerUser, loginUser, logoutUser };
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/user.controller.js';
+import { loginUser, registerUser, logoutUser } from '../controllers/user.controller.js';
+
 
 const router = Router();
 
 router.route('/register').post(registerUser);
+router.route('/login').post(registerUser);
+router.route('/logout').post(logoutUser);
+
 export default router ;
 
